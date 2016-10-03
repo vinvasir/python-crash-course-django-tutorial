3e92758b3a56f9ca7e8481ba486eee91040795c9
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-03 12:06:08 -0400

    wrote topic show template

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -14,6 +14,6 @@ def topics(request):
 
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by('-dated_added')
+    entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
