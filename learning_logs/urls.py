4598245eaa1a42824e915beef8cd2a52203cdd22
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-03 11:55:46 -0400

    wrote the topic show page url and view

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^topics/$', views.topics, name='topics'),
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
