7ca81c4ff843b9487af795591f22f1102256d161
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-03 22:26:38 -0400

    wrote login page with default view method

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+"""Defines URL patterns for users"""
+
+from django.conf.urls import url
+from django.contrib.auth.views import login
+
+from . import views
+
+urlpatterns = [
+    url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
+]
